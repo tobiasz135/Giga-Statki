792aff8d685db0933d44067f5eb187364674c3f5
Paweł Olejnik <235938@edu.p.lodz.pl>
2022-05-11 11:46:00 +0200

    Add background parts to buttons

--- src/main/java/com/example/test_javafx/HelloApplication.java
+++ src/main/java/com/example/test_javafx/HelloApplication.java
@@ -6,7 +6,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -19,9 +24,17 @@ public class HelloApplication extends Application {
         for (int i = 0; i < Serwer.HEIGHT; i++) {
             for (int j = 0; j < Serwer.WIDTH; j++) {
                 buttons[i][j] = new Button();
-                buttons[i][j].setMinWidth(32);
-                buttons[i][j].setMinHeight(32);
+                buttons[i][j].setMinWidth(34);
+                buttons[i][j].setMinHeight(34);
+                buttons[i][j].setMaxWidth(34);
+                buttons[i][j].setMaxHeight(34);
                 gridPane.add(buttons[i][j], j, i, 1, 1);
+                String PartNumber = new String("00");
+                PartNumber += (i * Serwer.WIDTH) + j + 1;
+                if(PartNumber.length() > 3) {
+                    PartNumber = PartNumber.substring(PartNumber.length() - 3);
+                }
+                buttons[i][j].setGraphic(new ImageView(String.valueOf(getClass().getResource("img/background_parts/image_part_" + PartNumber + ".png"))));
             }
         }
 
